1c7eaec22ecea36e0454019f5d3513e8e0441762
CaroPohn <carolinapohn@gmail.com>
2023-11-03 12:56:11 -0300

    Create player struct and declarations

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "header/raylib.h"
+
+namespace FlappyBird
+{
+	struct Player 
+	{
+		Vector2 position = { 0, 0 };
+		Texture2D texture;
+		Rectangle source = { 0, 0, 0, 0 };
+		float scale = 0.0f;
+		Rectangle dest = { 0, 0, 0, 0 };
+		Vector2 origin = { 0, 0 };
+	};
+
+	Player InitPlayer();
+	void UpdatePlayer();
+	void CheckMovementInput();
+	void DrawPlayer();
+}
