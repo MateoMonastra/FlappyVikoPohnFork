95bb2f249ecfdd65202dc11820ee739dc5a88aad
CaroPohn <carolinapohn@gmail.com>
2023-11-03 12:07:58 -0300

    Add scenes

---
+++ src/Game/Scenes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace FlappyBird
+{
+    enum class Scenes
+    {
+        Menu,
+        Play,
+        Pause,
+        Rules,
+        Credits,
+        WinScreen,
+        LoseScreen,
+        Exit
+    };
+}
