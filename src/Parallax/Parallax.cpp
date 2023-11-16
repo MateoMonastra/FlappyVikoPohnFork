6afea952aa83d5b4955c19c3d113ef702c790b43
Caro Pohn <carolinapohn@gmail.com>
2023-11-15 21:36:40 -0300

    Add parallax .h and .cpp with functions

---
+++ src/Parallax/Parallax.cpp
@@ -0,0 +1,58 @@
+#include "Parallax/Parallax.h"
+
+namespace FlappyBird
+{
+    Parallax InitParallax()
+    {
+        Parallax parallax;
+
+        parallax.backTexture = LoadTexture("res/parback.png");
+        parallax.middleTexture = LoadTexture("res/parmid.png");
+        parallax.frontTexture = LoadTexture("res/parfront.png");
+
+        return parallax;
+    }
+
+    void UpdateParallax(Parallax& parallax)
+    {
+        parallax.backXPos -= parallax.backSpeed * GetFrameTime();
+        parallax.middleXPos -= parallax.middleSpeed * GetFrameTime();
+        parallax.frontXPos -= parallax.frontSpeed * GetFrameTime();
+
+        if (parallax.backXPos <= -parallax.backTexture.width)
+        {
+            parallax.backXPos = 0;
+        }
+
+        if (parallax.middleXPos <= -parallax.middleTexture.width)
+        {
+            parallax.middleXPos = 0;
+        }
+
+        if (parallax.frontXPos <= -parallax.frontTexture.width)
+        {
+            parallax.frontXPos = 0;
+        }
+    }
+
+    void DrawParallax(Parallax& parallax)
+    {
+        Vector2 firstBack = { parallax.backXPos , 0 };
+        Vector2 secondBack = { parallax.backTexture.width + parallax.backXPos, 0 };
+
+        Vector2 firstMiddle = { parallax.middleXPos, 0 };
+        Vector2 secondMiddle = { parallax.middleTexture.width + parallax.middleXPos, 0 };
+
+        Vector2 firstFront = { parallax.frontXPos, 0 };
+        Vector2 secondFront = { parallax.frontTexture.width + parallax.frontXPos, 0 };
+
+        DrawTextureEx(parallax.backTexture, firstBack, parallax.rotation, parallax.scale, WHITE);
+        DrawTextureEx(parallax.backTexture, secondBack, parallax.rotation, parallax.scale, WHITE);
+
+        DrawTextureEx(parallax.middleTexture, firstMiddle, parallax.rotation, parallax.scale, WHITE);
+        DrawTextureEx(parallax.middleTexture, secondMiddle, parallax.rotation, parallax.scale, WHITE);
+
+        DrawTextureEx(parallax.frontTexture, firstFront, parallax.rotation, parallax.scale, WHITE);
+        DrawTextureEx(parallax.frontTexture, secondFront, parallax.rotation, parallax.scale, WHITE);
+    }
+}
---
+++ src/Parallax/Parallax.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Raylib/header/raylib.h"
+
+namespace FlappyBird
+{
+	struct Parallax
+	{
+		Texture2D backTexture = Texture2D();
+		Texture2D middleTexture = Texture2D();
+		Texture2D frontTexture = Texture2D();
+
+		float backSpeed = 30.0f;
+		float middleSpeed = 100.0f;
+		float frontSpeed = 400.0f;
+
+		float backXPos = 0.0f;
+		float middleXPos = 0.0f;
+		float frontXPos = 0.0f;
+
+		float yPos = 0.0f;
+
+		float rotation = 0.0f;
+		float scale = 1.0f;
+	};
+
+	Parallax InitParallax();
+	void UpdateParallax(Parallax& parallax);
+	void DrawParallax(Parallax& parallax);
+}
